a49fb32c6d0fd25b69776542fb0b55e1ffc40ebf
Jason Yeung <jsy9@sfu.ca>
2018-10-08 23:54:29 -0700

    edit syntax

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -1,20 +1,20 @@
 // UTIL
-export class Util {
-    static getCenter(el) {
+export const Util = {
+    getCenter: (el) => {
         let bbox = el.getBoundingClientRect();
-    
+
         return {
             x: bbox.left + (bbox.width/2),
             y: bbox.top + (bbox.height/2)
         };
-    }
-    
-    static getRadius(el) {
+    },
+
+    getRadius: (el) => {
         let bbox = el.getBoundingClientRect();
         return bbox.width/2;
-    }
+    },
 
-    static getOffset(el1, el2) {
+    getOffset: (el1, el2) => {
         return {
             x: (el2.x - el1.x),
             y: (el2.y - el1.y)
